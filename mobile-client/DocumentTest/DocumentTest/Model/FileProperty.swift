33928d0f0909ece2c04bc8b14bf0dd4e4790e5f0
YAANNY <46789462+YAANNY@users.noreply.github.com>
2019-02-03 21:21:44 +0000

    Create initial mobile client

---
+++ mobile-client/DocumentTest/DocumentTest/Model/FileProperty.swift
@@ -0,0 +1,22 @@
+//
+//  FileProperty.swift
+//  DocumentTest
+//
+//  Created by perrion huds on 28/01/2019.
+//  Copyright © 2019. All rights reserved.
+//
+
+import UIKit
+
+class FileProperty: NSObject
+{
+    var FileID : String = ""
+    var FileName : String = ""
+    var FileType: String = ""
+    var FileSize: String = ""
+    var FileTags: String = ""
+    var FileModifiedDate : String = ""
+    var FileEncryption: Bool = false
+    var FilePassword: String = "" 
+    
+}
