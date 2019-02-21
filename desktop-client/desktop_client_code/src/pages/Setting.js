c7abe126612fe7263d9f227e9082d3522aca7775
Danny Yao <dannyyaou@gmail.com>
2019-02-21 13:39:15 +0000

    handling change page with father function

---
+++ desktop-client/desktop_client_code/src/pages/Setting.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../css/main.css";
+
+class File extends React.Component {
+  render() {
+    return (
+      <div className="main-container">
+        <div className="files-container">
+          <p>Setting View</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default File;
