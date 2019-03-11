d65de0864736828bbbb58c3b9732f038ac290f22
Isaac Muhumuza <isaac.muhumuza@kcl.ac.uk>
2019-03-11 16:00:44 +0000

    FS Watcher

--- desktop-client/desktop_client_code/src/FsWatcher.js
+++ desktop-client/desktop_client_code/src/FsWatcher.js
@@ -1,29 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
-    <link 
-      rel="stylesheet" 
-      href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" 
-      integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" 
-      crossorigin="anonymous"
-    />
-    <meta name="theme-color" content="#000000" />
-
-    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-
-    <title>Benediction File System</title>
-  </head>
-  <body>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root"></div>
-    <script> //require('./src/FsWatcher.js') 
-      var folderWatch = require("./src/modules/DirectoryWatcher.js");
+var folderWatch = require("./modules/DirectoryWatcher.js");
 
 var fileMonitor = new folderWatch.DirectoryWatcher("C:\\benedictionFiles", true);
 
@@ -54,6 +29,3 @@ fileMonitor.on("fileRemoved", function (filePath) {
 // Let us know that directory monitoring is happening and where.
 console.log("Directory Monitoring of " + fileMonitor.root + " has started");
   
-    </script>
-  </body>
-</html>
