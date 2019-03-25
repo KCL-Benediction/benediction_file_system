b6a3215cfbedb2ca632b9e391d6da31a7aa86814
Isaac Muhumuza <isaac.muhumuza@kcl.ac.uk>
2019-03-25 14:27:34 +0000

    minor changes to read file

--- desktop-client/desktop_client_code/src/components/GetServerData.jsx
+++ desktop-client/desktop_client_code/src/components/GetServerData.jsx
@@ -76,6 +76,7 @@ class GetServerData extends React.Component {
     } , function(response) {
       response.on('end', () => {
         alert("Download Finish");
+        //write file details to json file to support locking
         fs.readFile("./public/files.json", 'utf-8', (error, content)=>{
           var obj = JSON.parse(content);
           obj[fileInfo.file_id] = fileInfo
